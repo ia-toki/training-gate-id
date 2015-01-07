dcad8f0f39b5744d08ca3397f570cd944fc756eb
William Gozali <will.gozali@gmail.com>
2015-01-07 17:20:02 +0700

    standarize code & text style in SESI3

--- program/SESI3/kuadrat3.pas
+++ program/SESI3/kuadrat3.pas
@@ -1,17 +1,17 @@
 var
-    a,b,c,x,hasil:longint;
+   a, b, c, x, hasil : longint;
 begin
-	// inisialisasi
-    a := 1;
-    b := 3;
-    c := -2;
-    
-    // baca nilai x
-    readln(x);
+   // inisialisasi
+   a := 1;
+   b := 3;
+   c := -2;
+   
+   // baca nilai x
+   readln(x);
 
-    // hitung hasil fungsi
-    hasil := a*sqr(x) + b*x + c;
+   // hitung hasil fungsi
+   hasil := a*sqr(x) + b*x + c;
 
-    // cetak
-    writeln('ax^2 + bx + c = ', hasil);
+   // cetak
+   writeln('ax^2 + bx + c = ', hasil);
 end.
