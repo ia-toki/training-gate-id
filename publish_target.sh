660ed165e9eade636b04b5d8f84a716e9e286c6c
Ashar Fuadi <fushar@gmail.com>
2015-06-06 13:50:21 +0700

    Skip Travis build when publishing target

--- publish_target.sh
+++ publish_target.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "ia-toki/training-gate-id" ] && [ "$TRAVIS_PULL_REQU
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
     git add *.pdf
-    git commit -m "Publish target from commit $TRAVIS_COMMIT"
+    git commit -m "Publish target from commit $TRAVIS_COMMIT" -m "[ci skip]"
     git push -f "https://${GH_TOKEN}@github.com/ia-toki/training-gate-id.git" master:target > /dev/null 2>&1
 
 fi
