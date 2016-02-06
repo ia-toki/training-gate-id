831e1c551a9e5651e50fd27f1952b3b0630101c0
William Gozali <will.gozali@gmail.com>
2016-02-06 10:58:02 +0700

    Cleanup setup.sh + install clrscode3e package

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ theme_path=`kpsewhich -var-value TEXMFLOCAL`/tex/latex/beamer
 mkdir -p $theme_path
 cp -R beamer-tokitex/beamerthemetokitex.sty beamer-tokitex/tokitex-asset $theme_path/
 
-/usr/share/texlive/texmf-dist/tex/latex/qtree/
-
 # Install lstaddons CTAN package
 lstaddons_path=`kpsewhich -var-value TEXMFLOCAL`/tex/latex/lstaddons
 if [ ! -d $lstaddons_path ]; then
@@ -19,8 +17,7 @@ if [ ! -d $lstaddons_path ]; then
 fi
 
 # Install qtree package
-qtree_path=`kpsewhich -var-value TEXMFLOCAL`/tex/latex/qtree/o
-echo $qtree_path
+qtree_path=`kpsewhich -var-value TEXMFLOCAL`/tex/latex/qtree
 if [ ! -d $qtree_path ]; then
     mkdir -p $qtree_path
 
@@ -28,6 +25,14 @@ if [ ! -d $qtree_path ]; then
     unzip qtree.zip
     cp qtree/* $qtree_path/
     rm -rf qtree*
+fi
+
+# Install clrscode3e package
+clrscode3e_path=`kpsewhich -var-value TEXMFLOCAL`/tex/latex/clrscode3e
+if [ ! -d $clrscode3e_path ]; then
+    mkdir -p $clrscode3e_path
+
+    cp ./lib/clrscode3e/* $clrscode3e_path
 fi
 
 # Reload
