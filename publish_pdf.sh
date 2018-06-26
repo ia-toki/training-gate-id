c2e2bd1c278c6987fe8b426fb3e341d73831084f
William Gozali <will.gozali@gmail.com>
2018-06-25 21:46:22 -0700

    Add publish pdf for PKD

--- publish_pdf.sh
+++ publish_pdf.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_REPO_SLUG" == "ia-toki/training-gate-id" ] && [ "$TRAVIS_PULL_REQU
     git init
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
-    git add pemrograman-dasar_*.pdf
+    git add *.pdf
     git commit -m "Publish PDFs from commit ia-toki/training-gate-id@$TRAVIS_COMMIT" -m "[ci skip]"
     git push -f "https://${GH_TOKEN}@github.com/ia-toki/training-gate-id-pdf.git" master > /dev/null 2>&1
 
