8fcfbaca4e2fa67dcc93a67d73a9ead8be91dc8b
William Gozali <will.gozali@gmail.com>
2018-07-22 21:57:44 -0700

    Add variabel

---
+++ topik/pemrograman-dasar-cpp/02-variabel-tipe-data/asset/assign.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int x;
+
+int main() {
+  x = 12;
+  printf("Nilai = %d\n", x);
+}
---
+++ topik/pemrograman-dasar-cpp/02-variabel-tipe-data/asset/tes.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main() {
+  short a = 32000;
+  unsigned short ua = 65355;
+  printf("%d %u\n", a, ua);
+}
