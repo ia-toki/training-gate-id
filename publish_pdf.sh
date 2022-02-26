d771fd33efc48da8fe821bdb5590768180406c73
Ashar Fuadi <fushar@gmail.com>
2022-02-26 10:49:22 +0700

    test

--- publish_pdf.sh
+++ publish_pdf.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Inspired by http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
-
-if [ "$TRAVIS_REPO_SLUG" == "ia-toki/training-gate-id" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-
-    cd target
-    git init
-    git config user.email "travis@travis-ci.org"
-    git config user.name "travis-ci"
-    git add *.pdf
-    git commit -m "Publish PDFs from commit ia-toki/training-gate-id@$TRAVIS_COMMIT" -m "[ci skip]"
-    git push -f "https://${GH_TOKEN}@github.com/ia-toki/training-gate-id-pdf.git" master > /dev/null 2>&1
-
-fi
+cd target
+git init
+git config init.defaultBranch master
+git config user.email "build@ia-toki.org"
+git config user.name "build"
+git add *.pdf
+git commit -m "Publish PDFs from commit ia-toki/training-gate-id@$GITHUB_SHA"
+git push -f "https://${GH_TOKEN}@github.com/ia-toki/training-gate-id-pdf.git" master > /dev/null 2>&1
